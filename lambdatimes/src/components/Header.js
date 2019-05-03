132a4798b7fc1f7462b7a420e417e032608e95ad
Ba-Zen <bazenatlas@gmail.com>
2019-05-03 14:03:18 -0400

    mvp

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 
-// Refactor this component to use styled components and not classNames. 
-// You can find the corresponding CSS in the CSS/index.css file
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: white;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
+const Heading = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: 700;
+  text-align: center;
+  color: black;
+`;
+
+const Date = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  margin-left: 25px;
+`;
+
+const Temp = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  text-align: right;
+  margin-right: 25px;
+`;
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
-  )
-}
+    <HeaderContainer>
+      <Date>SMARCH 32, 2018</Date>
+      <Heading>Lambda Times</Heading>
+      <Temp>98°</Temp>
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
